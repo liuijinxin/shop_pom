92555963fedeb1510809735b62b112f9bd9bce59
ljx <ljx@qq.com>
2018-11-21 17:32:45 +0800

    完成索引的头部，以及主页内容的导入

---
+++ shop_search/src/test/java/com/qf/shop_search/ShopSearchApplicationTests.java
@@ -0,0 +1,93 @@
+package com.qf.shop_search;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopSearchApplicationTests {
+
+    @Autowired
+    private SolrClient solrClient;
+
+    @Test
+    public void testAdd() throws IOException, SolrServerException {
+        /*SolrInputDocument document=new SolrInputDocument();
+        document.addField("id",1);
+        document.addField("title","美的冰箱");
+        document.addField("ginfo","实用的电器产品");
+        document.addField("gcount",100);
+        document.addField("gprice",200.5);
+        document.addField("gimage","www.baidu.com");
+        solrClient.add(document);
+        solrClient.commit();*/
+        for (int i = 1; i < 20; i++) {
+            SolrInputDocument document=new SolrInputDocument();
+            document.addField("id",i);
+            document.addField("gtitle","美的冰箱"+i);
+            document.addField("ginfo","实用的电器产品"+i);
+            document.addField("gcount",100+i);
+            document.addField("gprice",200.5+i);
+            document.addField("gimage","www.baidu.com"+i);
+            solrClient.add(document);
+        }
+        solrClient.commit();
+    }
+
+    @Test
+    public void testUpdate() throws IOException, SolrServerException {
+        SolrInputDocument document=new SolrInputDocument();
+        document.addField("id",0);
+        document.addField("gtitle","华为手机");
+        document.addField("ginfo","实用的电器产品");
+        document.addField("gcount",1500);
+        document.addField("gprice",1999);
+        document.addField("gimage","www.baidu123.com");
+        solrClient.add(document);
+        solrClient.commit();
+    }
+
+    @Test
+    public void testDelete() throws IOException, SolrServerException {
+        //根据id删除索引
+        /*solrClient.deleteById("0");
+        solrClient.commit();*/
+        //根据语句删除索引
+        /*solrClient.deleteByQuery("gtitle:美的冰箱2");
+        solrClient.commit();*/
+        //删除所有
+        solrClient.deleteByQuery("*:*");
+        solrClient.commit();
+    }
+
+    @Test
+    public void testQuery() throws IOException, SolrServerException {
+        SolrQuery solrQuery=new SolrQuery();
+        solrQuery.setQuery("gtitle:美的冰箱 || ginfo:电器");
+
+        QueryResponse response = solrClient.query(solrQuery);
+        SolrDocumentList results = response.getResults();
+        for (SolrDocument result : results) {
+            Integer id = Integer.parseInt(result.get("id")+"");
+            String title = (String) result.get("gtitle");
+            String ginfo = (String) result.get("ginfo");
+            Integer gcount = (Integer) result.get("gcount");
+            float price = (float) result.get("gprice");
+            System.out.println(id + "   " + title + "   " + ginfo + "   " + gcount + "   " + price);
+        }
+
+    }
+
+}
